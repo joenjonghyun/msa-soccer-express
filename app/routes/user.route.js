32b0e3629c5061e9f8a56ee0be850573607aacee
joenjonghyun <jjh980814@naver.com>
2022-04-20 18:54:28 +0900

    22.4.20. 19

--- app/routes/user.route.js
+++
@@ -1,7 +0,0 @@
-const { signup, userlist ,login } = require('../controllers/user.controller');
-const { verifyToken } = require('../routes/middlewares');
-module.exports = x => {
-    x.app.post(`${x.url}/signup`, signup)
-    x.app.post(`${x.url}/login`, verifyToken ,login)
-    x.app.get(`${x.url}/list`, userlist)
-}
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv'
 import express from 'express';
 import db from './app/models/index.js';
-import indexRouter from './app/routes/index.js';
 import apiRouter from "./app/routes/api.js"
+import articleRouter from "./app/routes/article.js"
+import basicRouter from "./app/routes/basic.js"
+import todoRouter from "./app/routes/todo.js"
+import userRouter from "./app/routes/user.js"
+import indexRouter from './app/routes/index.js';
 
 
 async function startServer() {
@@ -15,6 +19,10 @@ async function startServer() {
     app.use(express.json());
     app.use("/", indexRouter);
     app.use("/api", apiRouter)
+    app.use("/article", articleRouter)
+    app.use("/basic", basicRouter)
+    app.use("/todo", todoRouter)
+    app.use("/user", userRouter)
     db
         .mongoose
         .connect(mongoUri, {
@@ -28,6 +36,16 @@ async function startServer() {
             console.log(' 몽고DB와 연결 실패', err)
             process.exit();
         });
+    app.all("*", function(_req, res) {
+        return apiResponse.notFoundResponse(res, "페이지를 찾을 수 없습니다");
+        });
+        
+    app.use((err, _req, res) => {
+    if(err.name == "UnauthorizedError"){
+        return apiResponse.unauthorizedResponse(res, err.message);
+    }
+    });
+
     app.listen(port, () => {
         console.log('***************** ***************** *****************')
         console.log('********** 서버가 정상적으로 실행되고 있습니다 *********')
